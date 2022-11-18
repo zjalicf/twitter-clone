981bc7bded303fd3dc90d88f64a4b578c0db3c76
WhileTrue-Code <fsiljic13@gmail.com>
2022-11-18 22:36:45 +0100

    verification logic and determining userType logic moved to user_service and added setting one id for user and auth service.

--- Twitter-Backend/user_service/domain/user_store.go
+++ Twitter-Backend/user_service/domain/user_store.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type UserStore interface {
 	Get(id primitive.ObjectID) (*User, error)
 	GetAll() ([]*User, error)
-	Post(user *User) error
+	Post(user *User) (*User, error)
 }
