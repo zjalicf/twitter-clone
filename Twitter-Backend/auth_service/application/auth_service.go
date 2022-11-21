c862e95c9dcccbb11cf0d2683104ae4a22278938
nananeko1305 <nananeko1305@hotmail.com>
2022-11-21 14:45:30 +0100

    Created: Generating token and save it in localStorage

--- Twitter-Backend/auth_service/application/auth_service.go
+++ Twitter-Backend/auth_service/application/auth_service.go
@@ -5,16 +5,20 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 var (
 	userServiceHost = os.Getenv("USER_SERVICE_HOST")
 	userServicePort = os.Getenv("USER_SERVICE_PORT")
 )
+
+var jwtKey = []byte("my_secret_key")
 
 type AuthService struct {
 	store domain.AuthStore
@@ -65,18 +69,39 @@ func (service *AuthService) Register(user *domain.User) error {
 	return service.store.Register(&credentials)
 }
 
-func (service *AuthService) Login(credentials *domain.Credentials) (*domain.User, error) {
+func (service *AuthService) Login(credentials *domain.Credentials) string {
 
 	user, err := service.store.GetOneUser(credentials.Username)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return ""
 	}
 
 	passError := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(credentials.Password))
 
 	if passError != nil {
-		return nil, passError
+		fmt.Println(passError)
+		return ""
 	}
 
-	return user, nil
+	expirationTime := time.Now().Add(15 * time.Minute)
+
+	claims := &domain.Claims{
+		Username: user.Username,
+		Role:     user.UserType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return tokenString
 }
--- Twitter-Backend/auth_service/handlers/authHandler.go
+++ Twitter-Backend/auth_service/handlers/authHandler.go
@@ -4,19 +4,14 @@ import (
 	"auth_service/application"
 	"auth_service/domain"
 	"encoding/json"
-	"fmt"
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"time"
 )
 
 type AuthHandler struct {
 	service *application.AuthService
 }
-
-var jwtKey = []byte("my_secret_key")
 
 func NewAuthHandler(service *application.AuthService) *AuthHandler {
 	return &AuthHandler{
@@ -60,39 +55,6 @@ func (handler *AuthHandler) Login(writer http.ResponseWriter, req *http.Request)
 		return
 	}
 
-	user, err1 := handler.service.Login(&request)
-
-	if err1 != nil {
-		writer.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	expirationTime := time.Now().Add(15 * time.Minute)
-
-	claims := &domain.Claims{
-		Username: user.Username,
-		Role:     user.UserType,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	tokenString, err := token.SignedString(jwtKey)
-
-	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(tokenString)
-
-	http.SetCookie(writer, &http.Cookie{
-		Name:     "token",
-		Value:    tokenString,
-		Expires:  expirationTime,
-		HttpOnly: true,
-	})
+	writer.Write([]byte(handler.service.Login(&request)))
 
 }
