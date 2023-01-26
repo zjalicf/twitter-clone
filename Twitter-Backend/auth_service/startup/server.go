23b40dfa21bcc2a08a97b67b9d625be7da9f973e
zjalicf <64900037+zjalicf@users.noreply.github.com>
2023-01-26 14:08:30 +0100

    Feature logging (#33)
    * logging
    
    * fixing
    
    Co-authored-by: filip.zjalic <filip.zjalic@cyberlab.rs>

--- Twitter-Backend/auth_service/startup/server.go
+++ Twitter-Backend/auth_service/startup/server.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	saga "github.com/zjalicf/twitter-clone-common/common/saga/messaging"
 	"github.com/zjalicf/twitter-clone-common/common/saga/messaging/nats"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -27,6 +28,8 @@ import (
 	"time"
 )
 
+var Logger = logrus.New()
+
 type Server struct {
 	config *config.Config
 }
@@ -41,7 +44,45 @@ func NewServer(config *config.Config) *Server {
 	}
 }
 
+func initLogger() {
+	file, err := os.OpenFile("/app/logs/application.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	Logger.SetOutput(file)
+
+	rotationInterval := 24 * time.Hour
+	ticker := time.NewTicker(rotationInterval)
+	defer ticker.Stop()
+
+	go func() {
+		for range ticker.C {
+			rotateLogs(file)
+		}
+	}()
+}
+
+func rotateLogs(file *os.File) {
+	currentTime := time.Now().Format("2006-01-02_15-04-05")
+	err := os.Rename("/app/logs/application.log", "/app/logs/application_"+currentTime+".log")
+	if err != nil {
+		Logger.Error(err)
+	}
+	file.Close()
+
+	file, err = os.OpenFile("/app/logs/application.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		Logger.Error(err)
+	}
+
+	Logger.SetOutput(file)
+}
+
 func (server *Server) Start() {
+
+	initLogger()
+
 	mongoClient := server.initMongoClient()
 	defer func(mongoClient *mongo.Client, ctx context.Context) {
 		err := mongoClient.Disconnect(ctx)
